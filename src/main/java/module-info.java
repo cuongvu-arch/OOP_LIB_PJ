cbdb00c3293501d11b08802846b2244d7663130c
cuongvu-arch <24022279@vnu.edu.vn>
2025-04-02 14:51:52 +0700

    init commit hello world

---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module java.oop_library_project {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens java.oop_library_project to javafx.fxml;
+    exports java.oop_library_project;
+}
