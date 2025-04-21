5e3cd4a450bb04ac505ffe3893a68c2fa74b3011
cuongvu-arch <24022279@vnu.edu.vn>
2025-04-22 01:00:29 +0700

    resolve all conflict

--- src/main/java/Controller/HomePageScreenController.java
+++ src/main/java/Controller/HomePageScreenController.java
@@ -10,7 +10,7 @@ import javafx.scene.input.MouseEvent;
 import utils.SceneController;
 
 
-public class HomePageScreenController extends SceneController {
+public class HomePageScreenController {
     @FXML
     private ChoiceBox<String> adminFunction;
     private User currentUser;
@@ -33,8 +33,8 @@ public class HomePageScreenController extends SceneController {
         }
     }
 
-    public void follow (MouseEvent event) {
-        switchToFollowScreen(event);
+    public void follow () {
+        SceneController.getInstance().switchToScene("/followScreen.fxml");
     }
 
     public void switchToThuThu(ActionEvent event) {
