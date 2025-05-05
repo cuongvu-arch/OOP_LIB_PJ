15dba0622bb3855f9cbee73d96a38269b2e01f9a
lmd-2510 <lmduc7c1@gmail.com>
2025-05-06 03:08:52 +0700

    thêm modelview - bookRatingView
    đây là một package mới trong models, mục đích là trung gian giữa models và controllers

---
+++ src/main/java/models/viewmodel/BookRatingView.java
@@ -0,0 +1,28 @@
+package models.viewmodel;
+
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+import models.entities.Document;
+
+public class BookRatingView {
+    private final Document book;
+
+    private final StringProperty title;
+    private final StringProperty isbn;
+    private final StringProperty thumbnailUrl;
+
+    public BookRatingView(Document book) {
+        this.book = book;
+        this.title = new SimpleStringProperty(book.getTitle());
+        this.isbn = new SimpleStringProperty(book.getIsbn());
+        this.thumbnailUrl = new SimpleStringProperty(book.getThumbnailUrl());
+    }
+
+    public StringProperty titleProperty() { return title; }
+    public StringProperty isbnProperty() { return isbn; }
+    public StringProperty thumbnailUrlProperty() { return thumbnailUrl; }
+
+    public Document getBook() {
+        return book;
+    }
+}
