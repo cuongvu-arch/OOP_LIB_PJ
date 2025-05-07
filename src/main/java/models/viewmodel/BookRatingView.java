1e60c7dea252ee76c018dddaa4e4aab04af8e15f
lmd-2510 <lmduc7c1@gmail.com>
2025-05-07 11:22:38 +0700

    model view
    xóa đi những thành phần thừa

--- src/main/java/models/viewmodel/BookRatingView.java
+++ src/main/java/models/viewmodel/BookRatingView.java
@@ -6,22 +6,15 @@ import models.entities.Document;
 
 public class BookRatingView {
     private final Document book;
-
     private final StringProperty title;
     private final StringProperty isbn;
-    private final StringProperty thumbnailUrl;
 
     public BookRatingView(Document book) {
         this.book = book;
         this.title = new SimpleStringProperty(book.getTitle());
         this.isbn = new SimpleStringProperty(book.getIsbn());
-        this.thumbnailUrl = new SimpleStringProperty(book.getThumbnailUrl());
     }
 
     public StringProperty titleProperty() { return title; }
     public StringProperty isbnProperty() { return isbn; }
-    public StringProperty thumbnailUrlProperty() { return thumbnailUrl; }
-    public Document getBook() {
-        return book;
-    }
 }
