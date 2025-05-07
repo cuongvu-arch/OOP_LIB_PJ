125ebdb9dd8ad9d4dacc5da44fad219ca42ea407
lmd-2510 <lmduc7c1@gmail.com>
2025-05-08 00:53:35 +0700

    Services
    - BorrowHistoryService: thêm method và chỉnh sửa constructor, có hai method chính là trả về sách đã mượn và trả về sách đang mượn

--- src/main/java/models/services/BorrowHistoryService.java
+++ src/main/java/models/services/BorrowHistoryService.java
@@ -11,24 +11,47 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class BorrowHistoryService {
-
     private final DocumentDAO documentDAO;
+    private final BorrowRecordDAO borrowRecordDAO;
 
-    public BorrowHistoryService(DocumentDAO documentDAO) {
+    public BorrowHistoryService(DocumentDAO documentDAO, BorrowRecordDAO borrowRecordDAO) {
         this.documentDAO = documentDAO;
+        this.borrowRecordDAO = borrowRecordDAO;
     }
 
-    public List<Document> getBorrowHistory(Connection conn, int userId) throws SQLException {
-        List<BorrowRecord> records = BorrowRecordDAO.getByUserId(conn, userId);
-        List<Document> documents = new ArrayList<>();
+    public List<Document> getReturnedBooks(Connection conn, int userId) throws SQLException {
+        List<BorrowRecord> records = borrowRecordDAO.getByUserId(conn, userId);
+        List<Document> returnedDocuments = new ArrayList<>();
 
         for (BorrowRecord record : records) {
-            Document doc = documentDAO.getBookByIsbn(record.getIsbn());
-            if (doc != null) {
-                documents.add(doc);
+            // Kiểm tra xem có ngày trả hay không
+            if (record.getReturnDate() != null) {
+                Document doc = documentDAO.getBookByIsbn(record.getIsbn());
+                if (doc != null) {
+                    returnedDocuments.add(doc);
+                }
             }
         }
 
-        return documents;
+        return returnedDocuments;
+    }
+
+    // Lấy các sách chưa trả
+    public List<Document> getUnreturnedBooks(Connection conn, int userId) throws SQLException {
+        List<BorrowRecord> records = borrowRecordDAO.getByUserId(conn, userId);
+        List<Document> unreturnedDocuments = new ArrayList<>();
+
+        for (BorrowRecord record : records) {
+            // Kiểm tra xem chưa có ngày trả
+            if (record.getReturnDate() == null) {
+                Document doc = documentDAO.getBookByIsbn(record.getIsbn());
+                if (doc != null) {
+                    unreturnedDocuments.add(doc);
+                }
+            }
+        }
+
+        return unreturnedDocuments;
     }
 }
+
