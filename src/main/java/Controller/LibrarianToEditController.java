659fe75c7249c0f894d6b6dd791600962ed54bac
cuongvu-arch <24022279@vnu.edu.vn>
2025-05-08 21:17:24 +0700

    fix admin scene

--- src/main/java/Controller/LibrarianToEditController.java
+++ src/main/java/Controller/LibrarianToEditController.java
@@ -1,4 +1,14 @@
 package Controller;
 
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
 public class LibrarianToEditController {
+    public TextField addIsbnField;
+    public TextArea addBookInfoArea;
+    public TextField deleteIsbnField;
+    public TextArea deleteBookInfoArea;
+    public TextField editIsbnField;
+    public TextArea currentBookInfoArea;
+    public TextArea newBookInfoArea;
 }
